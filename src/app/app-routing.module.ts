06a088632873d2c8806316984a32477ef6b9dd99
alishba2 <alishbashazadi013@gmail.com>
2022-10-26 00:31:48 +0500

    components added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListComponent } from './list/list.component';
+import { LoginComponent } from './login/login.component';
+import { MoviesComponent } from './movies/movies.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { SongsComponent } from './songs/songs.component';
+import { AuthGuard } from './auth/authguard';
+import { SliderComponent } from './slider/slider.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'login' , component:LoginComponent},
+  {path:'signup' , component:SignUpComponent},
+
+  {path:'', canActivate:[AuthGuard], component:ListComponent , children:[
+    {path:'navbar' , component:NavbarComponent},
+    {path:'movies' , component:MoviesComponent},
+    {path:'songs', component:SongsComponent},
+    {path:'videos', component:SongsComponent},
+    {path:'slider' , component:SliderComponent}
+
+  ]},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,52 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { MoviesComponent } from './movies/movies.component';
+import { VideosComponent } from './videos/videos.component';
+import { SongsComponent } from './songs/songs.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { SliderComponent } from './slider/slider.component';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './list/list.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatCarouselModule } from '@ngmodule/material-carousel';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignUpComponent,
+    MoviesComponent,
+    VideosComponent,
+    SongsComponent,
+    NavbarComponent,
+    ListComponent,
+    SliderComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    CommonModule,
+    MatButtonModule,
+    MatSidenavModule,
+    // MatCarouselModule.forRoot()
+
+
+
+
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
